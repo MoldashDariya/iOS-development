4f48013bd5b333e930a0ef73f9239d3056505678
MoldashDariya <123226757+MoldashDariya@users.noreply.github.com>
2025-09-17 22:38:59 +0500

    Add files via upload

---
+++ assignment1_dariya.playground 2/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+// Step 1: Declare Variables for Personal Informa&on
+var firstName: String = "Dariya"
+var lastName: String = "Moldash"
+let currentYear: Int = 2025
+var birthYear: Int =  2005
+var age: Int = currentYear - birthYear
+var isStudent: Bool = true
+var height: Double = 1.65
+var hometown: String = "Almaty"
+var studyYear: Int = 4
+
+// Step 2: Create Variables for Your Hobbies and Interests
+var hobby: String = "Cooking"
+var numberOfHobbies: Int = 4
+var favoriteNumber: Int = 7
+var isHobbyCreative: Bool = true
+var specialty: String = "Information Systems"
+var graduationYear: Int = 2026
+var numberOfSiblings: Int = 2
+
+//Bonus
+var futureGoals: String = "🔥️️️️️️In the future, i want to make a lot of money and not work for the rest of my life📊️️️️️️."
+
+// Step 3: Create a Summary of Your Life Story
+var lifeStory: String = "My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear). I am currently a student: \(isStudent). I study \(specialty), and I am in my \(studyYear) year of university. My height is \(height) meters. I live in \(hometown). I have \(numberOfSiblings) siblings. I enjoy \(hobby), which is a creative hobby: \(isHobbyCreative). I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber). I plan to graduate in \(graduationYear). \(futureGoals)"
+
+
+// Step 4: Print Your Life Story
+print(lifeStory)
+
+
+
+
+
+
+
+
+
+
+
